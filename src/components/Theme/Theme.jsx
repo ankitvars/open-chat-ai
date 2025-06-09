b5d498fb4e9868844cd970f2fa3fba27248e997e
Ankit Varshney <varshneyankit011@gmail.com>
2025-06-09 22:11:25 +0530

    updated the css and theme issues

--- src/components/Theme/Theme.jsx
+++ src/components/Theme/Theme.jsx
@@ -1,6 +1,25 @@
 import styles from "./Theme.module.css";
+import { useEffect, useRef } from "react";
 
 export function Theme() {
+  const selectRef = useRef();
+
+  useEffect(() => {
+    // Detect system theme on mount
+    const isDark =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (isDark) {
+      selectRef.current.value = "dark";
+      document.documentElement.classList.remove("light", "dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      selectRef.current.value = "light";
+      document.documentElement.classList.remove("light", "dark");
+      document.documentElement.classList.add("light");
+    }
+  }, []);
+
   function handleValueChange(event) {
     const theme = event.target.value;
     document.documentElement.classList.remove("light", "dark");
@@ -15,13 +34,16 @@ export function Theme() {
       meta.setAttribute("content", theme);
     }
   }
-  
 
   return (
     <div className={styles.Theme}>
       <span>Theme:</span>
-      <select defaultValue="light dark" onChange={handleValueChange}>
-        <option value="light dark">System</option>
+      <select
+        ref={selectRef}
+        defaultValue="system"
+        onChange={handleValueChange}
+      >
+        <option value="system">System</option>
         <option value="light">Light</option>
         <option value="dark">Dark</option>
       </select>
